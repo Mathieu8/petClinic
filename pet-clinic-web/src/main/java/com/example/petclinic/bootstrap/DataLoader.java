92a9e31f0d9f6149f93b06d41f6a29004f904d62
Mathieu8 <mathieuvanommeren@gmail.com>
2021-05-28 13:50:27 +0200

    refractored map impl id property to be done automaticly. closes #23

--- pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/bootstrap/DataLoader.java
@@ -24,14 +24,13 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("a");
         owner1.setLastName("anna");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+
         owner2.setFirstName("b");
         owner2.setLastName("Bas");
         ownerService.save(owner2);
@@ -39,13 +38,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("loaded Owners........");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("z");
         vet1.setLastName("Zzz");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("y");
         vet2.setLastName("you");
         vetService.save(vet2);
