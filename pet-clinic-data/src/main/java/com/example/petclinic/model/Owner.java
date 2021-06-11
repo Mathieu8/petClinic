bcedbbd324c98bce3a06e817ba29038ba31d1332
mathieu8 <mathieuvanommeren@gmail.com>
2021-06-11 11:40:07 +0200

    added contact info to owner closes #28

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -4,7 +4,34 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Owner extends Person{
+    private String addres;
+    private String city;
+    private String tekephone;
     private Set<Pet> pets = new HashSet<>();
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTekephone() {
+        return tekephone;
+    }
+
+    public void setTekephone(String tekephone) {
+        this.tekephone = tekephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
