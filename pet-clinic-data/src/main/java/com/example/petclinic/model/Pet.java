08c83c6bcce81bca4fb36be29a626225ea669e3f
mathieu8 <mathieuvanommeren@gmail.com>
2021-06-14 10:02:19 +0200

    converted owners to JPA entities. closes #36

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
@@ -1,11 +1,22 @@
 package com.example.petclinic.model;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birthday")
     private Date birthday;
 
     public String getName() {
